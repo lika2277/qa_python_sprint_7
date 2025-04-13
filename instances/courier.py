441eeacb9a8add844d0328a8df4cd0f089ff039f
Liliana <lika2277@gmail.com>
2025-04-13 12:20:52 +0300

    Добавлены подписи allure

--- instances/courier.py
+++ instances/courier.py
@@ -1,10 +1,13 @@
 import random
 import string
+
+import allure
 import requests
 from data.data import url
 
 class Courier:
 
+    @allure.step("Генерация нового курьера")
     @staticmethod
     def generate_new_courier():
         def generate_random_string(length):
@@ -18,18 +21,21 @@ class Courier:
             "first_name": generate_random_string(10)
         }
 
+    @allure.step("Регистрация курьера")
     @staticmethod
     def register_new_courier(payload = None):
         if not payload:
             raise Exception('Courier not passed')
         return requests.post(url + '/api/v1/courier', data=payload)
 
+    @allure.step("Авторизация курьера")
     @staticmethod
     def login_courier(payload = None):
         if not payload:
             raise Exception('Courier login and password not passed')
         return requests.post(url + '/api/v1/courier/login', data=payload)
 
+    @allure.step("Удаление курьера")
     @staticmethod
     def delete_courier(payload = None):
         return requests.delete(url + '/api/v1/courier/' + (str(payload) if payload else ''))
--- instances/order.py
+++ instances/order.py
@@ -1,21 +1,27 @@
 import requests
+import allure
 from data.data import url
 
 class Order:
+
+    @allure.step("Создание заказа")
     @staticmethod
     def create_order(payload = None):
         if not payload:
             raise Exception('Order not passed')
         return requests.post(url + '/api/v1/orders', data=payload)
 
+    @allure.step("Получение списка заказов")
     @staticmethod
     def get_orders_list():
         return requests.get(url + '/api/v1/orders')
 
+    @allure.step("Получить заказ по треку")
     @staticmethod
     def get_order_by_track(track=None):
         return requests.get(url + '/api/v1/orders/track' + ('?t=' + str(track) if track else ''))
 
+    @allure.step("Принять заказ")
     @staticmethod
     def accept_order(order_id=None, courier_id=None):
         return requests.put(url + '/api/v1/orders/accept/' + (str(order_id) if order_id else '') + ('?courierId=' + str(courier_id) if courier_id else ''))
